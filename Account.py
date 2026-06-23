40ad398f8ef32f8063841feabd7be264124ea06e
Joelbakut7 <joelbakut7@gmail.com>
2026-06-23 03:21:31 +0100

    Create Account.py

---
+++ Account.py
@@ -0,0 +1,15 @@
+class Account:
+
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+        else:
+            print("Insufficient funds")
