7d6ea4efdc94b290917d49405cd4ba5251c5ab10
Joelbakut7 <joelbakut7@gmail.com>
2026-06-23 03:39:42 +0100

    Implemented encapsulation for account balance

--- Account.py
+++ Account.py
@@ -2,14 +2,23 @@ class Account:
 
     def __init__(self, owner, balance=0):
         self.owner = owner
-        self.balance = balance
+        self.__balance = balance
 
     def deposit(self, amount):
+
         if amount > 0:
-            self.balance += amount
+            self.__balance += amount
 
     def withdraw(self, amount):
-        if amount <= self.balance:
-            self.balance -= amount
+
+        if amount <= self.__balance:
+            self.__balance -= amount
+            print(
+                f"Withdrawal successful. "
+                f"Balance: {self.__balance}"
+            )
         else:
             print("Insufficient funds")
+
+    def get_balance(self):
+        return self.__balance
