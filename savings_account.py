81c58bd796e819fe979548b8310c240f31eed8e7
Joelbakut7 <joelbakut7@gmail.com>
2026-06-23 03:48:16 +0100

    Added inheritance overriding and withdrawal limit validation
    Refactor Savings class initialization and enhance withdrawal logic with print statements for limits. Update example usage to show balance before and after transactions.

--- savings_account.py
+++ savings_account.py
@@ -3,22 +3,50 @@ from Account import Account
 
 class Savings(Account):
 
-    def __init__(self, owner, balance=0, withdrawal_limit=100):
+    def __init__(self, owner,
+                 balance=0,
+                 withdrawal_limit=100):
+
         super().__init__(owner, balance)
+
         self.withdrawal_limit = withdrawal_limit
 
     def withdraw(self, amount):
 
         if amount > self.withdrawal_limit:
+
             print(
                 f"Withdrawal amount exceeds "
                 f"the limit of {self.withdrawal_limit}"
             )
+
         else:
             super().withdraw(amount)
 
 
-savings = Savings("Alice", 1000, 100)
+print("---Savings Account---")
 
-savings.withdraw(50)
-savings.withdraw(150)
+savings = Savings(
+    "Alice",
+    1000,
+    100
+)
+
+print(
+    f"Initial balance: "
+    f"{savings.get_balance()}"
+)
+
+savings.deposit(500)
+
+print(
+    f"Current balance: "
+    f"{savings.get_balance()}"
+)
+
+savings.withdraw(100)
+
+print(
+    f"Final balance: "
+    f"{savings.get_balance()}"
+)
